8a9c60409dd7c4a096e2ef806545d40560fdf9e0
vAayush3301 <vaayush3301@gmail.com>
2026-01-15 23:14:37 +0530

    Basic Login Page created

--- src/main/java/av/entrance/client/controller/LoginController.java
+++ src/main/java/av/entrance/client/controller/LoginController.java
@@ -1,4 +1,35 @@
 package av.entrance.client.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
+
 public class LoginController {
+    private static final String ADMIN_USERID = "admin0";
+    private static final String ADMIN_PWD = "test";
+    @FXML private TextField useridField;
+    @FXML private PasswordField passwordField;
+    @FXML private Label responseLabel;
+
+    @FXML
+    private void handleLogin() {
+        String user = useridField.getText();
+        String password = passwordField.getText();
+
+        if (user.isEmpty()) {
+            responseLabel.setText("Please enter your User ID");
+            return;
+        }
+
+        if (user.equals(ADMIN_USERID) && password.equals(ADMIN_PWD)) {
+            responseLabel.setText("Admin Logged In");
+        } else if (!password.isEmpty()){
+            responseLabel.setText("Invalid Password");
+        } else {
+            responseLabel.setText("User Logged in");
+        }
+    }
+
 }
