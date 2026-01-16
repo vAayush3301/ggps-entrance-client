f705e9c41b41cb6ab00107c7920750226c75ec6e
vAayush3301 <vaayush3301@gmail.com>
2026-01-17 00:48:22 +0530

    TestId added

--- src/main/java/av/entrance/client/model/Test.java
+++ src/main/java/av/entrance/client/model/Test.java
@@ -3,16 +3,21 @@ package av.entrance.client.model;
 import java.util.List;
 
 public class Test {
-
+    private String testId;
     private String testName;
     private List<Question> questions;
 
-    public Test(String testName, List<Question> questions) {
+    public Test(String testId, String testName, List<Question> questions) {
+        this.testId = testId;
         this.testName = testName;
         this.questions = questions;
     }
 
     public Test() {
+    }
+
+    public String getTestId() {
+        return testId;
     }
 
     public String getTestName() {
