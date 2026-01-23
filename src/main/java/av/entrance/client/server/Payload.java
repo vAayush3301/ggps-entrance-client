617849267cc327bf2695ee411f685117579915ad
vAayush3301 <vaayush3301@gmail.com>
2026-01-23 13:58:49 +0530

    Code Cleanup

--- src/main/java/av/entrance/client/server/Payload.java
+++ src/main/java/av/entrance/client/server/Payload.java
@@ -7,20 +7,17 @@ import java.util.List;
 
 public class Payload {
     private Test test;
-    private String type;
     private List<Response> responses;
 
     private String userId;
 
-    public Payload(String userId, List<Response> responses, String type) {
+    public Payload(String userId, List<Response> responses) {
         this.userId = userId;
         this.responses = responses;
-        this.type = type;
     }
 
-    public Payload(Test test, String type) {
+    public Payload(Test test) {
         this.test = test;
-        this.type = type;
     }
 
     public Payload() {
