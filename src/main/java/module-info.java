2706ef67eb859686e01ed39d3383c1b35485c847
vAayush3301 <vaayush3301@gmail.com>
2026-03-01 17:39:08 +0530

    Modular App

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module ExamDesk.av {
+    requires com.fasterxml.jackson.databind;
+    requires java.net.http;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires jdk.httpserver;
+    requires org.apache.poi.ooxml;
+    requires org.apache.poi.poi;
+}
