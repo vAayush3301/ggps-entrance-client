85276b293fc118f4e0b541eac8a94aed4d34bae9
vAayush3301 <vaayush3301@gmail.com>
2026-03-01 15:37:08 +0530

    Results added, partial setup

--- src/main/java/av/entrance/client/controller/user/items/TestRowController.java
+++ src/main/java/av/entrance/client/controller/user/items/TestRowController.java
@@ -1,10 +1,13 @@
 package av.entrance.client.controller.user.items;
 
+import av.entrance.client.model.SubmitResponse;
 import av.entrance.client.model.Test;
 import av.entrance.client.server.Server;
 import av.entrance.client.service.DeleteTestService;
+import av.entrance.client.service.GetResultsService;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -13,6 +16,7 @@ import javafx.scene.control.TextInputDialog;
 import java.net.BindException;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.List;
 import java.util.Optional;
 
 public class TestRowController {
@@ -124,5 +128,14 @@ public class TestRowController {
     }
 
     public void get_results() {
+        GetResultsService resultsService = new GetResultsService(test.getTestId());
+
+        resultsService.setOnSucceeded(workerStateEvent -> {
+            List<SubmitResponse> responses = resultsService.getValue();
+
+            System.out.println(responses);
+        });
+
+        resultsService.start();
     }
 }
---
+++ src/main/java/av/entrance/client/service/GetResultsService.java
@@ -0,0 +1,56 @@
+package av.entrance.client.service;
+
+import av.entrance.client.model.SubmitResponse;
+import av.entrance.client.model.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.List;
+
+public class GetResultsService extends Service<List<SubmitResponse>> {
+    private String testId;
+
+    public GetResultsService(String testId) {
+        this.testId = testId;
+    }
+
+    @Override
+    protected Task<List<SubmitResponse>> createTask() {
+        return new Task<>() {
+            @Override
+            protected List<SubmitResponse> call() throws IOException, InterruptedException {
+
+                try {
+                    HttpClient client = HttpClient.newHttpClient();
+
+                    HttpRequest request = HttpRequest.newBuilder()
+                            .uri(URI.create("https://ggps-entrance-xpiz.onrender.com/api/test/get_results?testId=" + testId))
+                            .timeout(Duration.ofSeconds(40))
+                            .GET()
+                            .build();
+
+                    HttpResponse<String> response =
+                            client.send(request, HttpResponse.BodyHandlers.ofString());
+                    System.out.println(response.body());
+
+                    ObjectMapper mapper = new ObjectMapper();
+
+                    SubmitResponse[] responsesArray = mapper.readValue(response.body(), SubmitResponse[].class);
+                    System.out.println(responsesArray);
+
+                    return List.of(responsesArray);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    throw e;
+                }
+            }
+        };
+    }
+}
